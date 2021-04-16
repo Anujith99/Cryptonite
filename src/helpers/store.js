871b62a3b613fda0d68634fc5db45f378595bdcf
Anujith99 <banuj99@gmail.com>
2021-04-16 18:46:25 +0530

    Basic redux setup done

---
+++ src/helpers/store.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, createStore, compose } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "reducers";
+
+const ReactReduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+export let store = createStore(
+  rootReducer,
+  compose(applyMiddleware(thunk), ReactReduxDevTools)
+);
