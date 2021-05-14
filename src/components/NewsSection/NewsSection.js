334471bcf7917d8f7419496d54026e8b03f800a0
Anujith99 <banuj99@gmail.com>
2021-05-14 19:07:36 +0530

    Modal created with portal

--- src/components/NewsSection/NewsSection.js
+++ src/components/NewsSection/NewsSection.js
@@ -1,13 +1,23 @@
 import React from "react";
 import Slider from "../Slider/Slider";
+import Modal from "../Modal/Modal";
 
 import { NEWS_TYPES } from "helpers/constants";
 
 class NewsSection extends React.Component {
+  state = {
+    showModal: false,
+  };
+
+  onDismiss = () => this.setState({ showModal: false });
+
   render() {
     return (
       <section id="news" className="section">
         <div className="container">
+          <button onClick={(e) => this.setState({ showModal: true })}>
+            Show Modal
+          </button>
           {this.props.showHeader ? (
             <>
               <h3 className="section-header">
@@ -22,6 +32,7 @@ class NewsSection extends React.Component {
             <Slider key={index} type={type} category={this.props.category} />
           ))}
         </div>
+        <Modal show={this.state.showModal} onDismiss={this.onDismiss} />
       </section>
     );
   }
